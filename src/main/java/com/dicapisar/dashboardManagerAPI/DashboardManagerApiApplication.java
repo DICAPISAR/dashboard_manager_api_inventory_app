039dc2d685ef7c985de4bbed1f77bb3e1a8b7adf
dicapisar <dicapisar@gmail.com>
2022-11-01 22:34:21 -0500

    create base project

---
+++ src/main/java/com/dicapisar/dashboardManagerAPI/DashboardManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.dicapisar.dashboardManagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicapisar/dashboardManagerAPI/DashboardManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicapisar.dashboardManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
